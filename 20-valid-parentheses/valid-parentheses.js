533b6b9bc98a99dc12c0aaac502dd4c9e68ff32c
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2023-08-21 21:05:14 -0500

    Time: 242 ms (5.04%) | Memory: 51.2 MB (5.04%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -3,29 +3,34 @@
  * @return {boolean}
  */
 var isValid = function(s) {
+  if(s.length <= 1) {
+    return false
+  } 
   const openingBrackets = new Set(["(", "[", "{"]);
   const correspondingClosingBracket = {
     "(": ")",
     "{": "}",
     "[": "]"
-  };
+};
 
   const closingBracketStack = [];
 
   for (let i = 0; i < s.length; i++) {
     const currBracket = s[i];
-
-    if (openingBrackets.has(currBracket)) {
+    console.log("currBracket", currBracket)
+    if(openingBrackets.has(currBracket)) {
       const closingBracket = correspondingClosingBracket[currBracket];
-      closingBracketStack.unshift(closingBracket);
+      console.log("closingBracket", closingBracket)
+      closingBracketStack.unshift(closingBracket)
+      console.log("closingBracketStack", closingBracketStack)
     } else {
       if (currBracket !== closingBracketStack[0]) {
+        console.log("closingBracketStack", closingBracketStack)
         return false;
       } else {
         closingBracketStack.shift();
       }
     }
   }
-
-  return closingBracketStack.length === 0;
+    return closingBracketStack.length === 0;
 };

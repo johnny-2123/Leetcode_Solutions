2596d14fe31b740ffa4da7c22d2050268ecb2406
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2023-08-22 12:26:04 -0500

    Time: 53 ms (94.22%) | Memory: 46.2 MB (8.86%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    let queue = [root];
+    let output = [];
+    let count = 0;
+
+    while(queue[0]) {
+        let qLength = queue.length;
+        let row = [];
+
+        for (let i = 0; i < qLength; i++) {
+            const currNode = queue.shift();
+            row.push(currNode);
+
+            if(currNode.left) queue.push(currNode.left)
+            if(currNode.right) queue.push(currNode.right)
+        }
+
+        if(row) {
+            output.push(row);
+            count++;
+        }
+    }
+
+    console.log("count", count)
+    return count
+    
+};

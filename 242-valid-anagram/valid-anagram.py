baf0ecd7c188f78adf6f3e0053a310c761f60245
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2023-08-28 15:16:28 -0500

    Time: 42 ms (45.86%) | Memory: 16.4 MB (5.28%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -14,15 +14,9 @@ class Solution(object):
         tSortedArr = sorted(t)
         tSorted = "".join(tSortedArr)
 
-        print(sSorted)
-        print(tSorted)
-
         for i in range(len(s)):
             sChar = sSorted[i]
             tChar = tSorted[i]
-            print(i)
-            print(sChar)
-            print(tChar)
             if tChar != sChar:
                 return False
 

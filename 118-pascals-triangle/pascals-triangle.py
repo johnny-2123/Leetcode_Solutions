a168fd41bac20930f2d3d7d1b7497cf9516a3774
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2023-09-04 18:34:39 -0500

    Time: 41 ms (65.92%) | Memory: 16.5 MB (5.96%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = [[1]]
+
+        for i in range (1, numRows):
+            arr = []
+            prevArr = res[i - 1]
+            j = 0
+
+            while j <= i:
+                num1 = prevArr[j - 1] if 0 <= (j - 1) < len(prevArr) else 0
+                num2 = prevArr[j] if 0 <= j < len(prevArr) else 0
+                arr.append(num1 + num2)
+                j += 1
+            res.append(arr)
+
+        print("res", res)
+        return res
+            

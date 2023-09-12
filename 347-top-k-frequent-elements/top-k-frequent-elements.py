30be50ed716e91c3a22d54f6d61505b04b63309d
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2023-09-11 19:33:18 -0500

    Time: 93 ms (87.98%) | Memory: 21 MB (85.55%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -3,10 +3,8 @@ from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         numCount = Counter(nums)
-        print("numCount", numCount)
         countNumPairs = [(count, num) for num, count in numCount.items()]
         countNumPairs.sort(reverse=True)
-        print("countNumPairs", countNumPairs)
         result = []
         i = 0
         while i < k:

c3d21cc2448eb6b448cb2193c756b568fbb75d6e
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2023-09-18 18:37:15 -0500

    Time: 177 ms (5.17%) | Memory: 20.2 MB (14.67%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.py
@@ -0,0 +1,47 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        maxArea = 0
+        currArea = 0
+
+        def _inBound(row, col):
+            
+            rowInBound = 0 <= row and row < len(grid)
+            colInBound = 0 <= col and col < len(grid[0])
+
+            return rowInBound and colInBound
+
+        def _dfs(row, col):
+            nonlocal currArea
+
+            if not _inBound(row, col):
+                return
+
+            if grid[row][col] == 0:
+                return
+            
+            pos = f'{row},{col}'
+            if pos in visited:
+                return
+        
+            visited.add(pos)
+
+            _dfs(row + 1, col)
+            _dfs(row - 1, col)
+            _dfs(row, col + 1)
+            _dfs(row, col - 1)
+            
+            if grid[row][col] == 1:
+                print(f"pos: {row}, {col}")
+                currArea += 1
+                print(f"currArea: {currArea}")
+
+        visited = set()
+
+        for row in range(0, len(grid)):
+            for col in range(0, len(grid[0])):
+                _dfs(row, col)
+                maxArea = max(maxArea, currArea)
+                currArea = 0
+        
+        return maxArea
+

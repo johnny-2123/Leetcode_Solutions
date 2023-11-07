391a9d582e89d8b22beff8114365a21e1c467e75
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2023-11-07 00:43:25 -0600

    Time: 86 ms (32.58%) | Memory: 55.2 MB (6.61%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -4,11 +4,11 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    const breakpoint = k % nums.length;
-    if (breakpoint === 0) return;
     const length = nums.length;
+    const breakpoint = k % length;
+    if (breakpoint === 0) return;
 
     const arr1 = nums.slice(-breakpoint);
     const arr2 = nums.slice(0, length - breakpoint);
-    nums.splice(0, length, ...nums.slice(-breakpoint), ...nums.slice(0, length - breakpoint));
+    nums.splice(0, length, ...arr1, ...arr2);
 };

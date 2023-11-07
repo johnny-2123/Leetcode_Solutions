f0aa1909bae7127a319f5b0098d53789de163150
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2023-11-06 22:28:36 -0600

    Time: 53 ms (50.48%) | Memory: 41.6 MB (90.96%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -7,18 +7,20 @@
  */
 var merge = function(nums1, m, nums2, n) {
     last = m + n - 1;
-    while (m > 0 && n > 0) {
-        if(nums1[m - 1] >= nums2[n - 1]) {
-            nums1[last] = nums1[m - 1]
+    m--;
+    n--;
+    while (m >= 0 && n >= 0) {
+        if(nums1[m] >= nums2[n]) {
+            nums1[last] = nums1[m]
             m--;
         } else{
-            nums1[last] = nums2[n - 1]
+            nums1[last] = nums2[n]
             n--;
         }  
         last--;
     }
-    while (n > 0) {
-        nums1[last] = nums2[n - 1]
+    while (n >= 0) {
+        nums1[last] = nums2[n]
         n--;
         last--;
     }

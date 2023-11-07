7c6ba018bb02b642c66b64f54ce736f4b7e24803
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2023-11-06 23:38:10 -0600

    Time: 146 ms (78.03%) | Memory: 17.9 MB (35.55%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -3,9 +3,6 @@ class Solution:
         nums.sort()
         count = 0
         currNum = nums[0]
-        print("nums", nums)
-        print("currNum", currNum)
-        print("n", len(nums)/2)
 
         for num in nums:
             if num != currNum:
@@ -13,8 +10,6 @@ class Solution:
                 currNum = num
             else:
                 count += 1
-            print("num", num)
-            print("count", count)
 
             if count > (len(nums)/2):
                 return currNum

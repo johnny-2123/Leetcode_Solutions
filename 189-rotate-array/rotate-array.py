03876a5741be26df2d04f150c848119e86546c5f
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2023-11-07 00:50:56 -0600

    Time: 190 ms (42.45%) | Memory: 27.6 MB (75.86%) - LeetSync

---
+++ 189-rotate-array/rotate-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        breakpoint = k % length
+        if breakpoint == 0:
+            return
+
+        arr1 = nums[length - breakpoint:]
+        arr2 = nums[:length - breakpoint]
+        print("arr1", arr1)
+        print("arr2", arr2)
+        nums[0:0+len(arr1)] = arr1
+        nums[breakpoint: breakpoint + (len(arr2))] = arr2
+
+        
+        

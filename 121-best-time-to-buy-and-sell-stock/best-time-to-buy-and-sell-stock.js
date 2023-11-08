43ea93c8a6f3b4474b43d8a61118836560e85e71
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2023-11-07 21:07:58 -0600

    Time: 80 ms (21.31%) | Memory: 51.3 MB (79.80%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -6,8 +6,9 @@ var maxProfit = function(prices) {
     let left = 0;
     let maxProfit = 0;
 
-    for (const [right, rightPrice] of prices.entries()) {
+    for (let right = 0; right < prices.length; right++) {
         const leftPrice = prices[left];
+        const rightPrice = prices[right]
         const currProfit = rightPrice - leftPrice;
         maxProfit = Math.max(currProfit, maxProfit);
 

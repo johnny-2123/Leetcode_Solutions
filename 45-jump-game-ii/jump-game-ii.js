a629377abbbca0617b4257f74dab1690e7772d6f
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2023-11-08 22:09:12 -0600

    Time: 66 ms (32.75%) | Memory: 44.4 MB (24.40%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.js
+++ 45-jump-game-ii/jump-game-ii.js
@@ -3,16 +3,13 @@
  * @return {number}
  */
 var jump = function(nums) {
-    if (nums.length <= 1) {
-        return 0
-    }
     let left = 0;
     let right = 0;
     let res = 0;
     while (right < nums.length - 1) {
         // keep track how far you can get from this window
         let maxIdx = 0;
-        for (let i = 0; i <= right; i++) {
+        for (let i = 0; i < right + 1; i++) {
             maxIdx = Math.max(maxIdx, i + nums[i]);
         };
         left = right + 1;

09cc5781311f89dc1779ea83e5f8ed23c24c07fd
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2023-11-13 22:57:40 -0600

    Time: 55 ms (88.77%) | Memory: 44.7 MB (67.97%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -6,17 +6,21 @@ var isPalindrome = function(s) {
     const trimmedString = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
     let [leftIdx, rightIdx] = [0, trimmedString.length - 1];
 
+
     while (leftIdx < trimmedString.length - 1) {
         const leftChar = trimmedString[leftIdx];
         const rightChar = trimmedString[rightIdx];
+
 
         if(leftChar !== rightChar){
             return false
         }
 
+
         leftIdx += 1;
         rightIdx -= 1;
-    } 
+    }
+
 
     return true
 };

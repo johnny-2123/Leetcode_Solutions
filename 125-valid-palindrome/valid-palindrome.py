b2f6c45618977c197100bb3798c7924b82f3bd22
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2023-11-13 23:01:40 -0600

    Time: 49 ms (63.58%) | Memory: 17.4 MB (34.05%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,17 +1,15 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         lowerCaseS = s.lower()
-        trimmedS = "".join(char for char in lowerCaseS if char.isalnum())
-        left, right = 0, len(trimmedS) - 1
+        trimmedStr = "".join(char for char in lowerCaseS if char.isalnum())
 
-        while left < len(trimmedS):
-            leftChar = trimmedS[left]
-            rightChar = trimmedS[right]
+        left, right = 0, len(trimmedStr) - 1
 
-            if leftChar != rightChar:
+        while left < len(trimmedStr):
+            if trimmedStr[left] != trimmedStr[right]:
                 return False
             
             left += 1
             right -= 1
-
+        
         return True

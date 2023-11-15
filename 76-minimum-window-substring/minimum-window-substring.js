74dbabb9a20186db598917633420ff0073989ec2
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2023-11-14 22:14:22 -0600

    Time: 84 ms (56.12%) | Memory: 47.5 MB (35.95%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.js
@@ -0,0 +1,66 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {string}
+ */
+var minWindow = function(s, t) {
+  if (t === "") return "";
+
+  let countT = {};
+  let window = {};
+
+  for (let i = 0; i < t.length; i++){
+      const char = t[i];
+      if (char in countT) {
+          countT[char]++; 
+      } else {
+          countT[char] = 1
+      }
+  }
+
+  let have = 0;
+  const need = Object.keys(countT).length; 
+
+  let res = [-1, -1];
+  let resLength = Infinity; 
+
+  let left = 0;
+
+  for (let right = 0; right < s.length; right++){
+      char = s[right];
+      if(char in window) {
+          window[char]++;
+      } else {
+          window[char] = 1;
+      }
+
+      if (countT[char] && (window[char] == countT[char])) {
+          have += 1;
+      }
+
+      while (have == need) {
+          if ((right - left + 1) < resLength) {
+              res = [left, right];
+              resLength = (right - left + 1)
+          }
+
+          const leftChar = s[left];
+          window[leftChar] -= 1; 
+
+          if(leftChar in countT && (window[leftChar] < countT[leftChar])) {
+              have -= 1;
+          }
+          
+          left += 1;
+      } 
+  }
+  
+  if (resLength !== Infinity) {
+      const [left, right] = res;
+      return s.slice(left, right + 1);
+    } else {
+          return ""
+    }
+
+
+};

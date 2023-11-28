33813339e5d2d8c765c84e6f5e7b0b875321aeaf
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2023-11-27 23:33:24 -0600

    Time: 61 ms (90.22%) | Memory: 49 MB (62.25%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.js
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.js
@@ -12,11 +12,9 @@
  * @return {TreeNode}
  */
 var searchBST = function(root, val) {
-    if (!root) return null
-    if (root.val === val) return root
-    if (val < root.val) {
-        return searchBST(root.left, val)
-    } else {
-        return searchBST(root.right, val)
-    }
+    if (!root) return null;
+    if (root.val === val) return root;
+
+    if (val < root.val) return searchBST(root.left, val);
+    return searchBST(root.right, val);
 };

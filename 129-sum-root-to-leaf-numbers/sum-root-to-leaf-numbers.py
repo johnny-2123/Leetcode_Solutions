def8008c1fbe0c8ad8a64ca28f430542e74ba6b5
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2023-12-04 18:41:56 -0600

    Time: 37 ms (70.80%) | Memory: 16.4 MB (24.03%) - LeetSync

---
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        if not root: return False
+
+        numbers = []
+
+        def _dfs(node, numStr = ""):
+            if not node: return
+            numStr += f"{node.val}"
+            
+            if not node.left and not node.right:
+                numbers.append(numStr)
+
+            _dfs(node.left, numStr)
+            _dfs(node.right, numStr)
+
+        _dfs(root)
+        
+        res = 0
+
+        for num in numbers:
+            res += int(num)
+
+        return res

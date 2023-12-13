d1954a8484ab4920a5ffdb96ea486e2ab8144fa4
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2023-12-12 21:32:45 -0600

    Time: 86 ms (6.20%) | Memory: 46.7 MB (6.98%) - LeetSync

---
+++ 86-partition-list/partition-list.ts
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function partition(head: ListNode | null, x: number): ListNode | null {
+    if (!head) return null;
+
+    const leftNodes: ListNode[] = [];
+    const rightNodes: ListNode[] = [];
+
+    let curr = head;
+    while (curr) {
+        if (curr.val < x) {
+            leftNodes.push(curr);    
+        } else {
+            rightNodes.push(curr);
+        }
+        const nextNode = curr.next;
+        curr.next = null;
+        curr = nextNode;
+    }
+
+    
+    let leftHead: ListNode = new ListNode(null); 
+    let leftTail: ListNode = leftHead
+    for (const node of leftNodes) {
+        leftTail.next = node;
+        leftTail = node;
+    }
+
+    let rightHead: ListNode = new ListNode(null);
+    let rightTail: ListNode = rightHead;
+    for (const node of rightNodes) {
+        rightTail.next = node;
+        rightTail = node;
+    }
+
+    console.log('leftHead', leftHead);
+    console.log('leftTail', leftTail);
+
+    console.log('rightHead', rightHead);
+    console.log('rightTail', rightTail);
+
+    if (!leftHead.next) {
+        leftHead.next = rightHead.next
+    } else {
+        leftTail.next = rightHead.next;
+    }
+
+    return leftHead.next
+};

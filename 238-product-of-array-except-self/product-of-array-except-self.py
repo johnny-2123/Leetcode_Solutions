b3bf26f5be0d48a08ef4ca453acb1a9aba8c13a2
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2024-01-02 13:47:16 -0600

    Time: 164 ms (95.54%) | Memory: 26.4 MB (6.71%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,26 +1,22 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        products = [nums[0]]
-        for i in range(1,len(nums)):
-            num = nums[i]
-            products.append(num * products[i - 1])
+        rightProducts = [0] * len(nums)
+        total = 1
+        for i in range(len(nums) - 1, -1, -1):
+            total *= nums[i]
+            rightProducts[i] = total
 
+        leftProducts = [0] * len(nums)
+        total = 1
+        for i in range(len(nums)):
+            total *= nums[i]
+            leftProducts[i] = total
+        
+        res = [0] * len(nums)
+        res[0] = 1 * rightProducts[1]
+        res[len(nums) - 1] = leftProducts[len(nums) - 2] * 1
 
-        reversedProducts = [nums[-1]]
-        for i in range(len(nums) - 1, 0, -1):
-            num = nums[i - 1]
-            reversedProducts.append(num * reversedProducts[-1])
-
-
-        reversedProducts.reverse()
-
-
-        res = []
-        for i in range(0, len(nums)):
-            num1 = products[i - 1] if i > 0 else 1
-            num2 = reversedProducts[i + 1] if i < len(nums) - 1 else 1
-            res.append(num1 * num2)
+        for i in range(1, len(nums) - 1):
+            res[i] = leftProducts[i - 1] * rightProducts[i + 1]
+        
         return res
-
-
-

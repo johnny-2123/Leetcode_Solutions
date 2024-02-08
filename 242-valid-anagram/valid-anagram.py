30fd1416d3bafc5b8ba455a4c352ec466da2923c
Johnny Avila <95261336+johnny-2123@users.noreply.github.com>
2024-02-07 21:36:26 -0600

    Time: 51 ms (58.46%) | Memory: 17.5 MB (51.66%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,24 +1,3 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-
-        if(len(s) != len(t)):
-            return False
-
-        sSortedArr = sorted(s)
-        sSorted = "".join(sSortedArr)
-        tSortedArr = sorted(t)
-        tSorted = "".join(tSortedArr)
-
-        for i in range(len(s)):
-            sChar = sSorted[i]
-            tChar = tSorted[i]
-            if tChar != sChar:
-                return False
-
-
-        return True
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
